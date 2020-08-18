b4ffdb3979ee8e814bf554e575f2df6068207e2b
Rance Campbell <rancewcampbell@gmail.com>
2020-08-18 10:58:02 -0600

    get environment variables working

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const axios = require('axios');
 const app = express();
+dotenv.config();
 const port = process.env.PORT || 3000;
+const API_KEY = process.env.API_KEY;
 
-app.get('/api/v1/stocks/percent-above/:ticker&:percent&:time', (req, res) => {
-  const { ticker, percent, time } = req.params;
-  console.log(ticker);
-  console.log(percent);
-  console.log(time);
-  const today = Math.floor(Date.now() / 1000);
-  const URL = `https://finnhub.io/api/v1/stock/candle?symbol=${ticker}&resolution=D&from=${time}&to=${today}&token=${process.env.API_KEY}`;
-  axios
-    .get(URL)
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  res.send('Hello World');
-});
+app.get(
+  '/api/v1/stocks/percent-above/ticker=:ticker&percent=:percent&time=:time',
+  (req, res) => {
+    const { ticker, percent, time } = req.params;
+    console.log(ticker);
+    console.log(percent);
+    console.log(time);
+    const today = Math.floor(Date.now() / 1000);
+    const URL = `https://finnhub.io/api/v1/stock/candle?symbol=${ticker}&resolution=D&from=${time}&to=${today}&token=${API_KEY}`;
+    axios
+      .get(URL)
+      .then((response) => {
+        console.log(response.data);
+        res.send(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
